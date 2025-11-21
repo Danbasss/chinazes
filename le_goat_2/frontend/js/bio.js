4bc9deaaaf5f4b5d2f21d456f7d9287f35729ab0
Danylo Khomyak <khomyak.danyly@gmail.com>
2025-11-21 14:07:46 +0200

    primary meme page

--- le_goat_2/frontend/js/bio.js
+++ le_goat_2/frontend/js/bio.js
@@ -1,5 +1,9 @@
-fetch("data/sample.json")
-.then(r => r.json())
-.then(data => {
-document.getElementById("bio").innerText = data.bio;
-});
+fetch("../data/bio.json")
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("bio").innerHTML = `
+          <h2>${data.full_name}</h2>
+          <p>${data.description}</p>
+      `;
+  })
+  .catch(error => console.error("Помилка JSON:", error));
--- le_goat_2/frontend/js/memes.js
+++ le_goat_2/frontend/js/memes.js
@@ -1,5 +1,26 @@
-const form = document.getElementById("memeForm");
-form.addEventListener("submit", e => {
-e.preventDefault();
-alert("Мем відправлено (поки що мок)");
+const grid = document.getElementById("memeGrid");
+const viewer = document.getElementById("viewer");
+const viewerImg = document.getElementById("viewerImg");
+
+// Завантажуємо JSON із мемами
+fetch("../data/memes.json")
+    .then(res => res.json())
+    .then(memes => {
+        memes.forEach(src => {
+            let img = document.createElement("img");
+            img.src = src;
+            img.addEventListener("click", () => openViewer(src));
+            grid.appendChild(img);
+        });
+    });
+
+// Fullscreen відкриття
+function openViewer(src) {
+    viewerImg.src = src;
+    viewer.classList.remove("hidden");
+}
+
+// Закриття по кліку
+viewer.addEventListener("click", () => {
+    viewer.classList.add("hidden");
 });
